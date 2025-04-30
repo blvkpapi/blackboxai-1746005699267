833afd880cb7d79631d7332a8b88acb907e2c44b
blvkpapi <prospermukuwe@gmail.com>
2025-04-30 11:35:32 +0200

    Add src/api/facebook.js 1746005719466

---
+++ src/api/facebook.js
@@ -0,0 +1,9 @@
+export async function fetchFacebookData(accessToken) {
+  // Placeholder function to fetch Facebook data using access token
+  // Implement API calls to Facebook Graph API here
+  return {
+    followers: 1500,
+    posts: 180,
+    engagementRate: 6.1,
+  };
+}
