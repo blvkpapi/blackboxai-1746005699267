ba358178ec9507d12073f90daddc3d66b19763cd
blvkpapi <prospermukuwe@gmail.com>
2025-04-30 11:35:31 +0200

    Add src/api/twitter.js 1746005716940

---
+++ src/api/twitter.js
@@ -0,0 +1,9 @@
+export async function fetchTwitterData(bearerToken) {
+  // Placeholder function to fetch Twitter data using bearer token
+  // Implement API calls to Twitter API here
+  return {
+    followers: 2000,
+    tweets: 300,
+    engagementRate: 4.8,
+  };
+}
