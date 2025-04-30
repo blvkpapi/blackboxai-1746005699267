7fbf3d6f73cffc1aa3ed83515e46acbc73e0ef9b
blvkpapi <prospermukuwe@gmail.com>
2025-04-30 12:29:49 +0200

    Add src/components/AnalyticsDashboard.js 1746008976179

--- src/components/AnalyticsDashboard.js
+++ src/components/AnalyticsDashboard.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { fetchInstagramData } from '../api/instagram';
+import { fetchTwitterData } from '../api/twitter';
+import { fetchFacebookData } from '../api/facebook';
 
 export default function AnalyticsDashboard() {
+  const [analyticsData, setAnalyticsData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      // Fetch data from APIs (using placeholder tokens here)
+      const instagram = await fetchInstagramData('instagram-access-token');
+      const twitter = await fetchTwitterData('twitter-bearer-token');
+      const facebook = await fetchFacebookData('facebook-access-token');
+
+      setAnalyticsData({
+        Instagram: instagram,
+        Twitter: twitter,
+        Facebook: facebook,
+      });
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <View className="p-4 flex-1">
       <Text className="text-lg mb-4">Analytics Dashboard</Text>
-      <Text>Charts and analytics will be displayed here.</Text>
-      {/* Placeholder for charts - can integrate libraries like react-native-chart-kit */}
+      <Text>Instagram Engagement Rate: {analyticsData.Instagram.engagementRate}%</Text>
+      <Text>Twitter Engagement Rate: {analyticsData.Twitter.engagementRate}%</Text>
+      <Text>Facebook Engagement Rate: {analyticsData.Facebook.engagementRate}%</Text>
+      {/* Placeholder for more detailed charts and analytics */}
     </View>
   );
 }
